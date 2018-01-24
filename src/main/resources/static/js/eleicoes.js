05cabb7e24963ef5b6f99bb907ab162d3b1810ef
Diogo Werner <diogowerner@gmail.com>
2018-01-24 17:30:24 -0200

    Botão de voltar e alterar turno em eleições

--- src/main/resources/static/js/eleicoes.js
+++ src/main/resources/static/js/eleicoes.js
@@ -25,6 +25,15 @@ var div = d3.select("body").append("div")
 
 var selected = "";
 
+d3.select("#voltar").on("click", function(d) {
+	if(selected!="") {
+		d3.selectAll(".pie").style("opacity", 1);
+		d3.selectAll(".arc > .porcentagem").style("opacity", 0);
+        svg.transition().duration(500).call(zoom.transform, d3.zoomIdentity);
+        d3.select("#voltar").style("display", "none");
+        }
+    })
+
 d3.json(arquivojson, function(error, mapa) {
 	render(error,mapa)
 });
@@ -164,12 +173,14 @@ function render(error,mapa) {
 	            zoomx=(d.x*(-zoomscale)+ width/2);
 	            zoomy=(d.y*(-zoomscale)+ width/4);
 	            svg.transition().duration(500).call(zoom.transform, d3.zoomIdentity.translate(zoomx, zoomy).scale(zoomscale));
+	            d3.select("#voltar").style("display", "block");
 	        } else {
 	         	selected = "";
 				d3.select(this.parentNode).selectAll(".pie").style("opacity", 1);
 				d3.select(this).selectAll(".arc > .porcentagem").style("opacity", 0);
 				d3.selectAll(".boundary-br").style("stroke-width", 1);
 	            svg.transition().duration(500).call(zoom.transform, d3.zoomIdentity);
+	            d3.select("#voltar").style("display", "none");
 	        }
         });
 }
